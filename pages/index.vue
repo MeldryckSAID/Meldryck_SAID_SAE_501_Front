3567d278143f719fa7daca0bb52836ca07f3ea7c
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2024-01-14 05:14:51 +0100

    feat(master)index

--- pages/index.vue
+++ pages/index.vue
@@ -1,61 +1,60 @@
+<script setup>
+
+
+/////////////////////////////////////////////////////////////////
+//Ajout de ref + vérification de la connexion entre le client et la bdd
+const apiData = ref([]);
+const apiData2 = ref([]);
+onBeforeMount(async () => {
+  try {
+    const response = await client.get('/pierres');
+    apiData.value = response.data;
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données de l\'API', error);
+  }
+});
+onBeforeMount(async () => {
+  try {
+    const response = await client.get('/montre');
+    apiData2.value = response.data;
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données de l\'API', error);
+  }
+});
+///////////////////////////////////////////////////////////////
+
+
+</script>
+
 <template>
-  <div class="background">
-    <!-- <div>
-      <h1>Hello helloooooo</h1>
+  <div>
+    <Header></Header>
 
-      <main class="layout__page"></main>
 
-      <ul>
-        <li v-for="item in apiDataboitier">{{ item.NomBoitier }}</li>
-      </ul>
-      <ul>
-        <li v-for="item in apiDatastones">{{ item.NomPierre }}</li>
-      </ul>
-    </div> -->
-    <div class="container-home">
-      <BtnMybtn to="/connexion"  >Inscription</BtnMybtn>
-      <BtnMybtn to="/connexion" variant="reverse" >Connexion</BtnMybtn>
-    </div>
+    
+    <!-- Appel des données dans le html pour tester-->
+    <br>
+    pierres:
+    <br>
+    <ul>
+      <li v-for="item in apiData" :key="item.id">
+        {{ item.nom }}
+        {{ item.prix }}€
+      </li>
+    </ul>
+    <br>
+    Montres :
+    <ul>
+      <li v-for="item in apiData2" :key="item.id">
+       <pre>{{ item }}</pre>
+      </li>
+    </ul>
+     <!-- Fin de l'appel des données dans le html pour tester-->
+
+
+
+
   </div>
 </template>
 
-<style lang="scss" scoped>
-.background {
-  background-color: black;
-  width: 100%;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  justify-content: center;
-}
-.container-home {
-  gap: 30px;
-  display: flex;
-  justify-content: center;
-}
-</style>
-
-<script setup>
-import { API } from "@/utils/axios";
-import MyIcon from "~/components/icon/MyIcon.vue";
-
-const apiDataboitier = ref([]);
-const apiDatastones = ref([]);
-
-const onboitier = async () => {
-  console.log(API);
-  const response = await API.get("/boitier");
-  apiDataboitier.value = response.data;
-};
-const onstones = async () => {
-  console.log(API);
-  const response = await API.get("/stones");
-  apiDatastones.value = response.data;
-};
-
-onMounted(async () => {
-  await onstones();
-  await onboitier();
-});
-</script>
+<style lang="scss" scoped></style>

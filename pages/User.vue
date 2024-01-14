3ee8846bd1edfdaf0a66ecc90359a4f6e6208015
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2024-01-14 01:49:05 +0100

    feat(master)fetch info user

--- pages/User.vue
+++ pages/User.vue
@@ -1,29 +1,53 @@
-<script setup>
-import { API } from "@/utils/axios";
-
-const apiDatauser = ref([]);
-
-onMounted(async () => {
-  await onuser();
-});
-
-const onuser = async () => {
-  console.log("test2");
-  const response = await API.get("/user");
-  apiDatauser.value = response.data;
-};
-</script>
 <template>
   <div>
-    <h1>user</h1>
+    <div v-if="userInfo">
+      <h3>Informations de l'utilisateur</h3>
+      <p>Nom : {{ userInfo.name }}</p>
+      <p>Prénom : {{ userInfo.surname }}</p>
+      <p>Email : {{ userInfo.mail }}</p>
+      <p>ID : {{ userInfo.UserID }}</p>
+    </div>
+    <div v-else>
+      <p>Utilisateur non connecté ou informations indisponibles</p>
+    </div>
+
+    <button v-if="isUserLoggedIn" @click="logout">Déconnexion</button>
   </div>
-  <ul>
-    <li v-for="item in apiDatauser">{{ item }}</li>
-    <li v-for="item in apiDatauser">{{ item.name }}</li>
-    <li v-for="item in apiDatauser">{{ item.Password }}</li>
-    <li v-for="item in apiDatauser">{{ item.surname }}</li>
-    <li v-for="item in apiDatauser">{{ item.mail }}</li>
-  </ul>
 </template>
 
-<style lang="scss"></style>
+<script setup>
+import { ref, onMounted, computed } from 'vue';
+import { useRouter } from 'vue-router';
+import { API } from "@/utils/axios";
+import { useGlobalStore } from "@/store/global.js";
+
+const store = useGlobalStore();
+const router = useRouter();
+const userInfo = ref(null);
+
+const isUserLoggedIn = computed(() => store.token !== null);
+
+const fetchUserInfo = async () => {
+  const userID = store.token;
+  if (userID) {
+    try {
+      const response = await API.get(`/user/${userID}`);
+      userInfo.value = response.data; // Ajustez selon le format de la réponse de votre API
+    } catch (error) {
+      console.error("Erreur lors de la récupération des informations de l'utilisateur :", error.message);
+    }
+  }
+};
+
+onMounted(fetchUserInfo);
+
+const logout = () => {
+  store.setToken(null);
+  localStorage.removeItem("token");
+  router.push("/connexion"); // Ou la route de votre choix
+};
+</script>
+
+<style>
+/* Votre style ici */
+</style>
--- pages/connexion.vue
+++ pages/connexion.vue
@@ -1,74 +1,240 @@
 <script setup>
 import { API } from "@/utils/axios";
 
-import { useGlobalStore } from '../store/global.js'
-const store = useGlobalStore()
+import { useGlobalStore } from "../store/global.js";
+const store = useGlobalStore();
 
-const router = useRouter()
-const userNew = ref({})
-const userCo = ref({})
-const message = ref("")
+const router = useRouter();
+const userNew = ref({});
+const userCo = ref({});
+const message = ref("");
+const userInfo = ref(null);
 
 // enregistrement de la montre modifiée dans la base de données
 const inscription = async () => {
-    try {
-        await API.post(`/inscription`, userNew.value);
-        message.value = "Vous êtes bien inscrit"
-    } catch (error) {
-        console.error("Erreur lors de l'inscription :", error.message)
-        message.value = "Erreur lors de l'inscription"
-    }
-}
+  try {
+    await API.post(`/inscription`, userNew.value);
+    message.value = "Vous êtes bien inscrit";
+  } catch (error) {
+    console.error("Erreur lors de l'inscription :", error.message);
+    message.value = "Erreur lors de l'inscription";
+  }
+};
 
 // enregistrement de la montre modifiée dans la base de données
-const connexion = async () => {
-    try {
-        const response = await API.post(`/login`, userCo.value)
-        const { token } = response.data // Récupérer le token depuis la réponse API
-        store.setToken(token) // Enregistrer le token dans le store Pinia
-        message.value = "Vous êtes bien connecté"
-        router.push('/user')
-    } catch (error) {
-        console.error("Erreur lors de la connexion :", error.message)
-        message.value = "Erreur lors de la connexion"
-    }
-}
+// const connexion = async () => {
+//   try {
+//     const response = await API.post(`/login`, userCo.value);
+//     const { token } = response.data; // Récupérer le token depuis la réponse API
+//     store.setToken(token); // Enregistrer le token dans le store Pinia
+//     message.value = "Vous êtes bien connecté";
+//     router.push("/user");
+//   } catch (error) {
+//     console.error("Erreur lors de la connexion :", error.message);
+//     message.value = "Erreur lors de la connexion";
+//   }
+// };
 
+// const fetchUserInfo = async () => {
+//   try {
+//     const token = store.token; // Récupérer le token du store
+//     if (token) {
+//       const response = await API.get(`/user/${token}`);
+//       userInfo.value = response.data[0]; // Supposons que l'API renvoie un tableau
+//     }
+//   } catch (error) {
+//     console.error("Erreur lors de la récupération des informations de l'utilisateur :", error.message);
+//   }
+// };
+
+const connexion = async () => {
+  try {
+    const response = await API.post(`/login`, userCo.value);
+    const { token } = response.data; // Récupérer le token depuis la réponse API
+    store.setToken(token); // Enregistrer le token dans le store Pinia
+    message.value = "Vous êtes bien connecté";
+    // Récupérer les informations de l'utilisateur
+    await fetchUserInfo(token);
+    //router.push("/user");
+  } catch (error) {
+    console.error("Erreur lors de la connexion :", error.message);
+    message.value = "Erreur lors de la connexion";
+  }
+};
+
+const fetchUserInfo = async (userID) => {
+  try {
+    const response = await API.get(`/user/${userID}`);
+    userInfo.value = response.data[0]; // Supposons que l'API renvoie un tableau
+  } catch (error) {
+    console.error(
+      "Erreur lors de la récupération des informations de l'utilisateur :",
+      error.message
+    );
+  }
+};
+
+onMounted(() => {
+  const token = store.token;
+  if (token) {
+    fetchUserInfo(token);
+  }
+});
+
+const isUserLoggedIn = computed(() => store.token !== null);
+
+const logout = async () => {
+  try {
+    // Envoyer une requête POST à la route /logout de l'API
+    await API.post("/logout");
+    // Nettoyer l'état de connexion côté client
+    store.setToken(null);
+    localStorage.removeItem("token");
+    router.push("/connexion");
+  } catch (error) {
+    console.error("Erreur lors de la déconnexion :", error.message);
+  }
+};
 </script>
 <template>
   <div>
     <h1>connexion</h1>
   </div>
-    <div>
-            <h2>Inscription</h2>
 
-            <form @submit.prevent="inscription" method="post">
-                <input type="text" name="name" id="name" required placeholder="name" v-model="userNew.name">
-                <input type="text" name="surname" id="surname" required placeholder="surname" v-model="userNew.surname">
-                <input type="text" name="mail" id="mail" required placeholder="mail" v-model="userNew.mail">
-                <input type="password" name="Password" id="Password" required placeholder="Mot de Passe" v-model="userNew.Password">
+  <div class="container">
+    <div class="container-form">
+      <h2>Inscription</h2>
 
-                <input type="submit" value="Je m'inscris">
-            </form>
+      <form
+        class="form-inscription"
+        @submit.prevent="inscription"
+        method="post"
+      >
+        <input
+          class="form-inscription-input"
+          type="text"
+          name="name"
+          id="name"
+          required
+          placeholder="name"
+          v-model="userNew.name"
+        />
+        <input
+          class="form-inscription-input"
+          type="text"
+          name="surname"
+          id="surname"
+          required
+          placeholder="surname"
+          v-model="userNew.surname"
+        />
+        <input
+          class="form-inscription-input"
+          type="text"
+          name="mail"
+          id="mail"
+          required
+          placeholder="mail"
+          v-model="userNew.mail"
+        />
+        <input
+          class="form-inscription-input"
+          type="password"
+          name="Password"
+          id="Password"
+          required
+          placeholder="Mot de Passe"
+          v-model="userNew.Password"
+        />
 
-            {{ userNew }}
-        </div>
+        <input
+          class="form-inscription-btn"
+          type="submit"
+          value="Je m'inscris"
+        />
+      </form>
 
-        <div>
-            <h2>Connexion</h2>
+      
+    </div>
 
-            <form @submit.prevent="connexion" method="post">
-                <input type="text" name="mail" id="mail" required placeholder="mail" v-model="userCo.mail">
-                <input type="password" name="Password" id="Password" required placeholder="Mot de Passe" v-model="userCo.Password">
+    <div class="container-form">
+      <h2>Connexion</h2>
 
-                <input type="submit" value="Je me connect">
-            </form>
+      <form class="form-co" @submit.prevent="connexion" method="post">
+        <input
+          class="form-co-input"
+          type="text"
+          name="mail"
+          id="mail"
+          required
+          placeholder="mail"
+          v-model="userCo.mail"
+        />
+        <input
+          class="form-co-input"
+          type="password"
+          name="Password"
+          id="Password"
+          required
+          placeholder="Mot de Passe"
+          v-model="userCo.Password"
+        />
 
-            {{ userCo }}
-        </div>
+        <input class="form-co-btn" type="submit" value="Je me connect" />
+      </form>
 
-        <p>{{ message }}</p>
+      
+    </div>
+  </div>
+  <div v-if="userInfo">
+    <h3>Informations de l'utilisateur</h3>
+    <p>Nom : {{ userInfo.name }}</p>
+    <p>Prénom : {{ userInfo.surname }}</p>
+    <p>Email : {{ userInfo.mail }}</p>
+    
+  </div>
+  <div v-else>
+    <p>Utilisateur non connecté ou informations indisponibles</p>
+  </div>
 
+  <div v-if="isUserLoggedIn">
+    <button @click="logout">Déconnexion</button>
+  </div>
+
+  <p>{{ message }}</p>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.container {
+  display: flex;
+  justify-content: space-around;
+  &-form {
+    width: fit-content;
+    padding-left: 5%;
+    padding-right: 5%;
+    border-radius: 5%;
+    color: white;
+    flex-direction: column;
+    background-color: #393e43;
+    justify-content: center;
+    align-items: center;
+  }
+}
+
+.form-co,
+.form-inscription {
+  display: flex;
+  flex-direction: column;
+
+  gap: 16px;
+  &-input {
+    width: fit-content;
+    padding: 5px;
+    border: 2px solid black;
+    border-radius: 5%;
+  }
+  &-btn {
+    background-color: aqua;
+  }
+}
+</style>
